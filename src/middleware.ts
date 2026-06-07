71e4f6fca29bb0a4fba8eed1d5fa686afc7ab066
도욱상 맥북 <dowooksang@dougsang-ui-MacBookPro.local>
2026-06-07 19:57:16 +0900

    Fire the middleware bouncer forever

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export function middleware() {
-  // 🚨 경호원 전원 해고 및 철수! 무조건 VIP 대시보드로 통과시킵니다.
+  // 🚨 경호원 전원 해고! 묻지도 따지지도 않고 무조건 통과!
   return NextResponse.next();
 }
 
